1cd4bca6107f78a083007911425e72e97568b323
Твоя Булочка <85406297+Romb1k@users.noreply.github.com>
2021-06-11 10:52:49 +0300

    Add files via upload

---
+++ Rabbits and Recurrence Relations.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+
+long long rabbitPairs(int numMonths, int numOffspring) {
+	if (numMonths == 1) {
+		return 1;
+	}
+	else if (numMonths == 2) {
+		return numOffspring;
+	}
+	long long oneGen = rabbitPairs(numMonths - 1, numOffspring);
+	long long twoGen = rabbitPairs(numMonths - 2, numOffspring);
+	if (numMonths <= 4) {
+		return (oneGen + twoGen);
+	}
+	return (oneGen + (twoGen * numOffspring));
+}
+
+int main() {
+
+	int numMonths, numOffspring;
+	cin >> numMonths >> numOffspring;
+	cout << rabbitPairs(numMonths, numOffspring);
+}
